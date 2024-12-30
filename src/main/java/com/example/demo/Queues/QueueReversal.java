f1f41266178078ebbd0f4cd383c002c4cede7edf
spiderlord04 <spiderlord04@gmail.com>
2024-12-30 16:59:37 +0000

    updated Qeues

--- src/main/java/com/example/demo/Queues/QueueReversal.java
+++ src/main/java/com/example/demo/Queues/QueueReversal.java
@@ -7,10 +7,37 @@ import java.util.*;
 public class QueueReversal {
     public static void reverse(Queue<Integer> q){
         Stack<Integer> s = new Stack<>();
-        
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+            
+        }
     }
 
     public static void main(String[] args) {
-//jitu        
+//jitu         
+    Queue<Integer> q = new LinkedList<>(); 
+        //or
+        //Queue<Integer> q = new ArrayDeque<>(); 
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+
+        System.out.println(q);
+
+        reverse(q);
+        System.out.println(q);
+
     }
 }
